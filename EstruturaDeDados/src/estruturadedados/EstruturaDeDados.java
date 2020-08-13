e6214867f8b433710e526d2ff730f4dc02a6e940
Vitor Santos Braga <vittor_braga@hotmail.com>
2020-08-12 21:28:55 -0300

    Initial

--- EstruturaDeDados/src/estruturadedados/EstruturaDeDados.java
+++ EstruturaDeDados/src/estruturadedados/EstruturaDeDados.java
@@ -1,18 +1,9 @@
 package estruturadedados;
 
+import java.util.Hashtable;
+
 public class EstruturaDeDados {
     public static void main(String[] args) {
         // TODO code application logic here
-        //Array arr = new Array();
-        Pilha pilha = new Pilha();
-        int numero = 29;
-        while(numero>0){
-            pilha.push(numero%2);
-            numero = numero/2;
-        }
-        while(pilha.size()>0){
-            System.out.println(pilha.pop());
-        }
-    }
-    
+    }   
 }
